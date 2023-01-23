d74ca44f331195ee9bf7e5bac9ce6b2a4dabeae4
Mila <martins.fm28@outlook.com>
2023-01-23 10:55:27 -0300

    Cotação com filtro dinâmico de pesquisa de produto

--- models/product_extension.py
+++ models/product_extension.py
@@ -7,7 +7,15 @@ class ProductExtension(models.Model):
     @api.model
     def name_search(self, name, args=None, operator='ilike', limit=100):
         args = args or []
+        name_split = name.split()
+        array = []
+        for palavra in name_split:
+            array.append('|')
+            array.append('|')
+            array.append(('name', operator, palavra))
+            array.append(('product_template_attribute_value_ids',operator,palavra))
+            array.append(('fipe_ids',operator,palavra))
         if name:
-            pesquisa = self.search(['|','|',('name',operator,name),('product_template_attribute_value_ids',operator,name),('fipe_ids',operator,name)])
+            pesquisa = self.search(array)
             return pesquisa.name_get()
         return self.search([('name',operator,name)]+args, limit=limit).name_get()
